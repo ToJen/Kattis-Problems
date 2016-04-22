51dda700732c825da9939cd58d822cbb063d68ed
ToJen <tomisinjenrola@gmail.com>
2016-04-22 17:44:32 -0230

    First commit

---
+++ Carrots/Carrots.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Carrots
+{
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int N = in.nextInt(), P = in.nextInt();
+		String desc = in.next();
+		System.out.println(P);
+	}
+}

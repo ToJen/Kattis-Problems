71395f6296c0f5d9544f5d60164d8a2d09033538
Tomisin <tomisinjenrola@gmail.com>
2016-08-17 13:41:18 -0230

    Solved with C++

---
+++ Bijele/Bijele.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+	int k, q, r, b, n, p;
+
+	cin >> k >> q >> r >> b >> n >> p;
+
+	cout << 1-k << " "
+		 << 1-q << " "
+		 << 2-r << " "
+		 << 2-b << " "
+		 << 2-n << " "
+		 << 8-p << "\n";
+
+	return 0;
+}

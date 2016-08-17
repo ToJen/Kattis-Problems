1e24394d5dd10fa4a9e71c641e00416b4a43128a
Tomisin <tomisinjenrola@gmail.com>
2016-08-17 15:26:37 -0230

    Solved it!

---
+++ Zamka/Zamka.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <limits.h>
+
+using namespace std;
+
+int main()
+{
+
+	int l, d, x, n=INT_MAX, m=0, sum=0;
+	cin >> l >> d >> x;
+
+	for(int i=l; i<=d; ++i) {
+		string s = to_string(i);
+		for(int j=0; j<s.length(); ++j) {
+			sum += (s[j]- '0');
+		}
+		if(sum==x) {
+			if(i < n) n = i;
+			if(i > m) m = i;
+		}
+		sum=0;
+	}
+	
+	cout << n << ' ' << m << endl;
+
+	return 0;
+}

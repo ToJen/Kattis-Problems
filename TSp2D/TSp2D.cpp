25386a98d403ce1b5324ab68214afaf3e33142f0
Tomisin <tomisinjenrola@gmail.com>
2016-08-22 15:52:52 -0230

    More edits

--- TSp2D/TSp2D.cpp
+++ TSp2D/TSp2D.cpp
@@ -4,24 +4,30 @@
 */
 
 #include <iostream>
-#include <ctime>
+#include <ctime>	
 
 using namespace std;
 
 int main()
 {
 	clock_t start;
-	double d;
+	double dur;
 	start = clock();
 
 
-	int n, x, y;
+	int n, dist;
+	double x, y, opt, val, naive;
+
+	cin >> n;
+	for(int i=0; i<n; ++i) {
+
+	}
+
+	dist = round( sqrt(  ) )
 
 
-	d = (clock() - start) / (double) CLOCKS_PER_SEC;
-	cout << d << endl;
+	dur = (clock() - start) / (double) CLOCKS_PER_SEC;
+	cout << dur << endl;
 
 	return 0;
 }
-
-/////////////////////////// add header comments to other files made today
